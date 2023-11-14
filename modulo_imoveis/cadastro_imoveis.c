f39b181572f39171f3a8a826c676e09234a038e6
devv-thiago <tfofano3@gmail.com>
2023-11-13 21:27:55 -0300

    ajuste de erros modulo imoveis

--- modulo_imoveis/cadastro_imoveis.c
+++ modulo_imoveis/cadastro_imoveis.c
@@ -1,75 +1,99 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include <unistd.h>
 
 #define IMOVEL_ARQ "./Data/imoveis.txt"
 
-struct Imovel {
-	int id;
-	char tipo[50];
-	char endereco[100];
-	char cliente[100];
-	float preco;
-};
+int gerarNovoID() {
+	FILE *arquivo = fopen(IMOVEL_ARQ, "rb");
+	int numRegistros = 0;
+
+	if (arquivo == NULL) {
+		printf("Erro ao abrir o arquivo\n");
+		return 0;
+	}
+
+	while (fgetc(arquivo) != EOF) {
+		char c = fgetc(arquivo);
+		if (c == '\n') {
+			numRegistros++;
+		}
+	}
+	fclose(arquivo);
+
+	int novoID = numRegistros + 1;
+
+	return novoID;
+}
+
 
 int cadastrarImovelArq(int idImovel, char endereco[100], char tipo[50], float preco) {
 	FILE *arquivo = fopen(IMOVEL_ARQ, "a+");
-	if(arquivo == NULL) {
+
+	if (arquivo == NULL) {
 		printf("Erro ao abrir o arquivo!");
 		return 0;
+	}
+
+	fprintf(arquivo, "%i %s %s %f\n", idImovel, endereco, tipo, preco);
+
+	fclose(arquivo);
+	return 1;
+}
+
+void cadastrarImovel() {
+	int idImovel;
+	char endereco[100], tipo[50];
+	float preco;
+
+	idImovel = gerarNovoID();
+	printf("Digite o tipo do imóvel: ");
+	scanf("%s", tipo);
+	getchar();
+	printf("Digite o endereço do imóvel: ");
+	scanf("%s", endereco);
+	printf("Digite o valor do imóvel: ");
+	scanf("%f", &preco);
+	getchar();
+
+	int imovelCadastrado = cadastrarImovelArq(idImovel, endereco, tipo, preco);
+
+	if (imovelCadastrado == 1) {
+		printf("Imóvel cadastrado com sucesso!\n");
 	} else {
-		fprintf(arquivo, "%i %s %s %f", idImovel, endereco, tipo, preco);
-		return 1;
+		printf("Erro ao cadastrar imóvel!\n");
+	}
+}
+
+void listarImoveis() {
+	FILE *arquivo = fopen(IMOVEL_ARQ, "r");
+
+	if (arquivo == NULL) {
+		printf("Erro ao abrir o arquivo %s\n", IMOVEL_ARQ);
+		return;
+	}
+
+	printf("\nLista de Imóveis:\n");
+	printf("ID\tEndereço\tTipo\tPreço\n");
+
+	while (!feof(arquivo)) {
+		int idImovel;
+		char endereco[100], tipo[50];
+		float preco;
+
+		fscanf(arquivo, "%i %s %s %f", &idImovel, endereco, tipo, &preco);
+		if (!feof(arquivo)) {
+			printf("%i\t%s\t%s\t%.2f\n", idImovel, endereco, tipo, preco);
+		}
 	}
 
 	fclose(arquivo);
 }
 
-void cadastrarImovel(struct Imovel imoveis[], int *totalImoveis) {
-	if (*totalImoveis < 100) {
-		struct Imovel novoImovel;
-		novoImovel.id = (*totalImoveis) + 1;
-		printf("Digite o tipo do imóvel: ");
-		scanf("%s", novoImovel.tipo);
-		printf("Digite o endereço do imóvel: ");
-		scanf(" %[^\n]", novoImovel.endereco);
-		printf("Digite o código do cliente: ");
-		scanf(" %[^\n]", novoImovel.cliente);
-		printf("Digite o preço do imóvel: ");
-		scanf("%lf", &novoImovel.preco);
-
-		int imovelCadastrado = cadastrarImovelArq(novoImovel.id, novoImovel.endereco, novoImovel.tipo, novoImovel.preco);
-		if (imovelCadastrado == 1) {
-			printf("Imóvel cadastrado com sucesso!");
-		} else {
-			printf("Erro ao cadastrar imóvel!");
-		}
-
-		imoveis[*totalImoveis] = novoImovel;
-		(*totalImoveis)++;
-	} else {
-		printf("O limite de imóveis cadastrados foi atingido. Não é possível cadastrar mais imóveis.\n");
-	}
-}
-
-
-void listarImoveis(const struct Imovel imoveis[], int totalImoveis) {
-	printf("Lista de imóveis cadastrados:\n");
-	for (int i = 0; i < totalImoveis; i++) {
-		printf("ID: %i\n", imoveis[i].id);
-		printf("Tipo: %s\n", imoveis[i].tipo);
-		printf("Endereço: %s\n", imoveis[i].endereco);
-		printf("Cliente: %s\n", imoveis[i].cliente);
-		printf("Preço: %.2lf\n", imoveis[i].preco);
-		printf("\n");
-	}
-}
-
 void MenuImovel() {
-	struct Imovel imoveis[100];
-	int totalImoveis = 0;
-
 	int opcao;
+
 	do {
 		system("cls");
 		printf("IMÓVEIS");
@@ -83,11 +107,17 @@ void MenuImovel() {
 
 		switch (opcao) {
 			case 1:
-				cadastrarImovel(imoveis, &totalImoveis);
+				system("cls");
+				printf("IMÓVEIS");
+				printf("\n\n");
+				cadastrarImovel();
 				sleep(1);
 				break;
 			case 2:
-				listarImoveis(imoveis, totalImoveis);
+				system("cls");
+				printf("IMÓVEIS");
+				printf("\n\n");
+				listarImoveis();
 				break;
 			case 0:
 				system("cls");
@@ -97,3 +127,4 @@ void MenuImovel() {
 		}
 	} while (opcao != 0);
 }
+
